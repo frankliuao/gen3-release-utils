429131454449b6aeedba73854524d7096b7e9983
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2023-06-01 10:30:22 -0400

    Update gen3-qa-monthly-release-notes-generator_scheduled.sh

--- jenkins-jobs/gen3-qa-monthly-release-notes-generator_scheduled.sh
+++ jenkins-jobs/gen3-qa-monthly-release-notes-generator_scheduled.sh
@@ -11,9 +11,9 @@ pip3 install --editable git+https://github.com/uc-cdis/release-helper.git@gen3re
 
 export PATH=$PATH:/home/jenkins/.local/bin:/home/jenkins/.local/lib
 
-START_DATE=`date --date="41 day ago" +%Y-%m-%d`
-END_DATE=`date --date="14 day ago" +%Y-%m-%d`
-RELEASE_VERSION=`date --date="$END_DATE +1 month" +%Y.%m`
+START_DATE="2023-04-20"
+END_DATE="2023-05-12"
+RELEASE_VERSION="2023.06"
 RELEASE_NAME="Core Gen3 Release $RELEASE_VERSION"
 
 echo "### Generating Release Notes ###"
