50a9dfa5c4582a7105cfc307e89ffe40302b7635
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-09-05 10:17:23 -0700

    add jobs to select and release namespaces for CI

---
+++ jenkins-jobs/ci-only-select-and-lock-namespace.groovy
@@ -0,0 +1,74 @@
+/*
+    String parameter AVAILABLE_NAMESPACES
+        Comma-separated list of available namespaces to run integration tests
+        Default - ""
+        e.g., jenkins-brain,jenkins-dcp,jenkins-genomel
+    String parameter REPO
+        e.g., gitops-qa
+    String parameter BRANCH
+        e.g. chore/update_portal_qabrh
+*/
+pipeline {
+    agent {
+      node {
+        label 'gen3-qa-worker'
+      }
+    }
+    stages {
+        stage('Clean old workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Initial setup') {
+            steps {
+                // cloud-automation
+                checkout([
+                  $class: 'GitSCM',
+                  branches: [[name: 'refs/heads/master']],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'cloud-automation']],
+                  submoduleCfg: [],
+                  userRemoteConfigs: [[credentialsId: 'PlanXCyborgUserJenkins', url: 'https://github.com/uc-cdis/cloud-automation.git']]
+                ])
+            }
+        }
+        stage('Select and lock namespace') {
+            steps {
+                dir("select-and-lock-namespace") {
+                    script {
+                        sh '''#!/bin/bash +x
+                            export GEN3_HOME=\$WORKSPACE/cloud-automation
+                            source \$GEN3_HOME/gen3/gen3setup.sh
+                            times=0
+                            IFS=',' read -ra namespaces <<< "$AVAILABLE_NAMESPACES"
+                            while [ "$times" -ne 120 ]; do
+                                # Try to find an unlocked namespace
+                                for ((i=0; i<${#namespaces[@]}; i++)); do
+                                    export KUBECTL_NAMESPACE="${namespaces[$i]}"
+                                    echo "attempting to lock namespace $KUBECTL_NAMESPACE with a wait time of 1 minute"
+                                    klockResult=$(bash "$GEN3_HOME/gen3/bin/klock.sh" "lock" "jenkins" "$REPO_$BRANCH" 10800 -w 60)
+                                    if [[ $klockResult -eq 0 ]]; then
+                                        echo "Selected namespace $KUBECTL_NAMESPACE"
+                                        echo "$KUBECTL_NAMESPACE" > namespace.txt
+                                        return 0
+                                    else
+                                        # Unable to lock a namespace
+                                        echo "no available workspace, yet..."
+                                    fi
+                                done
+                                times=$((times + 1))
+                                sleep 60
+                            done
+                        '''
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'select-and-lock-namespace/gitops.json'
+        }
+    }
+}
